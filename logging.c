5921eac3ae93a01ba12896bd2d65b2d2ca1abb1d
Chuck Tilbury <chucktilbury@gmail.com>
2018-11-15 23:50:17 -0600

    logging mostly working

---
+++ logging.c
@@ -0,0 +1,189 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+#include <stdint.h>
+#include <errno.h>
+#include <time.h>
+#include "logging.h"
+
+static FILE *outfp[4];
+static int debug_level = 0; // no debugging messages
+static const char *type_strs[] = {
+    "DEBUG", // debug messages appear at level 2
+    "ENTER", // enter and return appear at level 5
+    "RETURN",
+    "INFO", // info messages appear at level 1
+    "WARN", // the rest of these always appear
+    "ERROR",
+    "SYNTAX",
+    // internal and fatal end the program
+    // internal errors are error conditions that should "never happen"
+    "INTERNAL",
+    // fatal errors are conditions such as not being able to open a file
+    "FATAL",
+};
+
+void set_debug_level(int lev)
+{
+    debug_level = lev;
+}
+
+void clean_log_file(void)
+{
+    if (outfp[1] != NULL)
+        fclose(outfp[1]);
+}
+
+void init_logging(logging_flags_t flags, ...)
+{
+    va_list args;
+
+    memset(outfp, 0, sizeof(outfp));
+
+    if (flags & LOG_STDOUT)
+        outfp[0] = stdout;
+
+    // if both stdout and stderr are selected, then stderr is used.
+    if (flags & LOG_STDERR)
+        outfp[0] = stderr;
+
+    if (flags & LOG_FILE)
+    {
+        va_start(args, flags);
+        char *fname = va_arg(args, char *);
+        va_end(args);
+        char *mode;
+        if (flags & LOG_APPEND)
+            mode = "a";
+        else
+            mode = "w";
+
+        if (NULL == (outfp[1] = fopen(fname, mode)))
+        {
+            fprintf(stderr, "FATAL ERROR: Cannot open the logfile \"%s\": %s\n", fname, strerror(errno));
+            exit(1);
+        }
+        atexit(clean_log_file);
+    }
+}
+
+void write_buffer(const char *buffer, size_t blen)
+{
+    if (outfp[0] != NULL)
+        fwrite(buffer, 1, blen, outfp[0]);
+    if (outfp[1] != NULL)
+        fwrite(buffer, 1, blen, outfp[1]);
+}
+
+void write_args(const char *str, va_list args)
+{
+    char buffer[1024];
+
+    vsnprintf(buffer, sizeof(buffer), str, args);
+    write_buffer(buffer, strlen(buffer));
+}
+
+void write_str(const char *str, ...)
+{
+    va_list args;
+    char buffer[1024];
+
+    va_start(args, str);
+    vsnprintf(buffer, sizeof(buffer), str, args);
+    va_end(args);
+
+    write_buffer(buffer, strlen(buffer));
+}
+
+void show_verbose_start(int type, const char *func, int line)
+{
+
+    time_t ti;
+    struct tm *t;
+
+    time(&ti);
+    t = localtime(&ti);
+
+    write_str("[%d/%d/%d %d:%d:%d] %s: %s: %d: ",
+              t->tm_mon, t->tm_mday, t->tm_year + 1900,
+              t->tm_hour, t->tm_min, t->tm_sec,
+              type_strs[type], func, line);
+}
+
+void show_start(int type)
+{
+    write_str("%s: ", type_strs[type]);
+}
+
+
+void show_msg(typelog_t type, int lev, const char *func, int line, const char *fmt, ...)
+{
+
+    va_list args;
+
+    if (type == DEBUG || type == ENTER || type == RETURN)
+    {
+        if (lev <= debug_level)
+        {
+            show_verbose_start(type, func, line);
+            va_start(args, fmt);
+            write_args(fmt, args);
+            va_end(args);
+            write_str("\n");
+        }
+    }
+    else if (type == INTERNAL)
+    {
+        show_verbose_start(type, func, line);
+        va_start(args, fmt);
+        write_args(fmt, args);
+        va_end(args);
+        write_str("\n");
+        exit(1);
+    }
+    else if (type == INFO)
+    {
+        if (lev <= debug_level)
+        {
+            show_start(type);
+            va_start(args, fmt);
+            write_str(fmt, args);
+            va_end(args);
+            write_str("\n");
+        }
+    }
+    else
+    {
+        show_start(type);
+        va_start(args, fmt);
+        write_str(fmt, args);
+        va_end(args);
+        write_str("\n");
+        if (type == FATAL)
+            exit(1);
+    }
+}
+
+#if 0
+int main(void)
+{
+
+    init_logging(LOG_STDOUT | LOG_FILE, "logfile.txt");
+    set_debug_level(10);
+
+    DEBUG(5, "This is a message: %d", 123);
+    set_debug_level(5);
+    DEBUG(6, "This is a message: %d", 1234);
+    ENTER();
+    RET();
+    SYNTAX("this is a syntax error");
+    INFO("this is a info");
+    set_debug_level(0);
+    INFO("this is a info");
+    ENTER();
+    RET();
+    return 0;
+}
+#endif
---
+++ logging.h
@@ -0,0 +1,44 @@
+#ifndef _LOGGING_H_
+#define _LOGGING_H_
+
+typedef enum typelog_t
+{
+    DEBUG,
+    ENTER,
+    RETURN,
+    INFO,
+    WARN,
+    ERROR,
+    SYNTAX,
+    INTERNAL,
+    FATAL,
+} typelog_t;
+
+typedef enum logging_flags_t
+{
+    LOG_STDOUT = 0x01,
+    LOG_STDERR = 0x02,
+    LOG_FILE = 0x04,
+    LOG_APPEND = 0x08,
+} logging_flags_t;
+
+#ifdef _DEBUGGING
+#define DEBUG(lev, fmt, ...) show_msg(DEBUG, (lev), __func__, __LINE__, fmt __VA_OPT__(, ) __VA_ARGS__)
+#define ENTER() show_msg(ENTER, 5, __func__, __LINE__, "")
+#define VRET(v) show_msg(RETURN, 5, __func__, __LINE__, ##v)
+#define RET() show_msg(RETURN, 5, __func__, __LINE__, "")
+#else
+#define DEBUG(lev, fmt, ...)
+#define ENTER()
+#define VRET(v)
+#define RET()
+#endif
+
+#define INFO(fmt, ...) show_msg(INFO, 1, __func__, __LINE__, fmt __VA_OPT__(, ) __VA_ARGS__)
+#define WARN(fmt, ...) show_msg(WARN, 0, __func__, __LINE__, fmt __VA_OPT__(, ) __VA_ARGS__)
+#define ERROR(fmt, ...) show_msg(ERROR, 0, __func__, __LINE__, fmt __VA_OPT__(, ) __VA_ARGS__)
+#define SYNTAX(fmt, ...) show_msg(SYNTAX, 0, __func__, __LINE__, fmt __VA_OPT__(, ) __VA_ARGS__)
+#define INTERNAL(fmt, ...) show_msg(INTERNAL, 0, __func__, __LINE__, fmt __VA_OPT__(, ) __VA_ARGS__)
+#define FATAL(fmt, ...) show_msg(FATAL, 0, __func__, __LINE__, fmt __VA_OPT__(, ) __VA_ARGS__)
+
+#endif /* _LOGGING_H_ */
