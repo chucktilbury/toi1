89708a4610adda8df000e829c194cf1a2a279fd1
Chuck Tilbury <chucktilbury@gmail.com>
2018-11-29 09:13:11 -0600

    adding parser functionality

--- class_def.c
+++ class_def.c
@@ -1,9 +1,26 @@
+/*
+    Handle a class definition.
+*/
 
 #include "logging.h"
 #include "errors.h"
 
+
+/*
+    When this is entered, a name has been encountered that gived the name of 
+    the class is being defined. Before any other actions are taken the name 
+    needs be retreived and the context pushed.
+
+    The class definition has the form of
+    name(inherit1, inherit2, ...) {class body}
+
+    The inherited classes must already have a definition. The class body 
+    consists of variable definitions and function definitions.
+*/
 void do_class(void)
 {
     ENTER();
     RET();
 }
+
+// classes can have a scope operator? 
--- import_def.c
+++ import_def.c
@@ -1,9 +1,51 @@
+/*
+    Handle imports. 
+
+    When do_import is called an "import" token has been scanned. The tokens 
+    that follow is a single symbol that is reduced to a file to read. There
+    is a system path that is used to find the files, similar to the way python 
+    handles imports. The string ".toi" is appended and the file is opened for 
+    scanning. A new context is also pushed (without the file extention) and the 
+    symbols in the file are accessed using the context.
+
+    Example:
+    import file1;
+*/
+#include <stdio.h>
+#include <string.h>
 
 #include "logging.h"
 #include "errors.h"
+#include "scanner.h"
+
+#define FNAME_SIZE 1024
+
+static void decorate_import(const char *token, char *buf)
+{
+    // stubbed
+    strcpy(buf, token);
+    strcat(buf, ".toi");
+}
 
 void do_import(void)
 {
     ENTER();
+    token_t tok;
+    char fname[FNAME_SIZE];
+
+    tok = get_token();
+    if (tok == SYMBOL_TOK)
+    {
+        push_context(get_token_string());
+        decorate_import(get_token_string(), fname);
+        open_file(fname);
+
+        tok = get_token();
+        if (tok != SEMI_TOK)
+            expect_token(SEMI_TOK, tok, NULL);
+    }
+    else
+        expect_token(SYMBOL_TOK, tok, "import failed");
+
     RET();
 }
